647f54b896ccf9e9ab0757e6f1c9060fe2ca041a
SaverioCascelli <saverio.cascelli@gmail.com>
2022-10-12 16:44:13 +0200

    [import] ticket calculation function

--- js/script.js
+++ js/script.js
@@ -1,32 +1,41 @@
+const pricePerKm = .21;
+const underAgePrice = .2;
+const over65Price = .4;
+let ageDiscount = 0;
+let ticketCategory ="Biglietto Standard";
+let totalPrice;
+let ticketPrice = "";
+
+
 
 const btnForm = document.getElementById("btn-form");
 btnForm.addEventListener("click" , function(){
     const name = document.getElementById("input-name").value;
     const tripLenght = parseInt(document.getElementById("input-km").value);
     const ageCategory = document.getElementById("age").value;
+    
+    
 
+    if(ageCategory === "underAge"){
+     ageDiscount = underAgePrice;
+     ticketCategory = "Biglietto minori";
+    }
+    else if(ageCategory === "over65") {
+        ageDiscount = over65Price;
+        ticketCategory = "Biglietto over 65"
+        
+    }
+
+    totalPrice = tripLenght * pricePerKm ;
+    totalPrice -= (totalPrice * ageDiscount);
+    ticketPrice = totalPrice.toFixed(2)+"€";
     
-    
+    document.querySelector("#passengerName").innerHTML = name;
+    document.querySelector("#ticketPrice").innerHTML = ticketPrice;
+    document.querySelector("#ticketDiscount").innerHTML = ticketCategory;
+
+    document.getElementById("ticket-price").classList.remove("d-none");
 });
 
-// const passengerAge = parseInt(prompt("Età passeggero:"));
-// const tripLenght = parseInt(prompt("Lunghezza viaggio in Km:"));
-
-// const pricePerKm = .21;
-// const underAge = 18;
-// const overAge = 65;
-// const under18Price = .2;
-// const over65Price = .4;
-// let ageDiscount = 0;
-// let totalPrice;
-// let ticketPrice = "";
 
 
-// if(passengerAge < underAge) ageDiscount = under18Price;
-// else if( passengerAge > overAge) ageDiscount = over65Price;
-
-// totalPrice = tripLenght * pricePerKm ;
-
-// totalPrice -= (totalPrice * ageDiscount);
-
-// ticketPrice = totalPrice.toFixed(2);
